a3bef9e556a723c5eb67feedc514bd2cd181a02c
vincent <18wjchen1@stu.edu.cn>
2021-04-30 09:38:04 +0800

    version 0.0.2

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## Features
 
-替换markdown文本文件中的部分中文字符为对应英文字符
+替换 markdown 文本文件中的部分中文字符为对应英文字符
+
+```javascript
+const CP = ["，", "：", "？", "！", "。", "“", "”", "（", "）"];
+const EP = [",", ":", "?", "!", ".", '"', '"', "(", ")"];
+```
+
+## Usage
+
+1. ctrl+shift+p 調出命令行，輸入 'markdown cp format'
+2. alt+f 快捷鍵
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,35 @@
-import * as vscode from 'vscode';
-const CP = ['，','：','？','、','！','。','“','”','（','）'];
-const EP = [',',':','?','.','!','。','"','"','(',')'];
+import * as vscode from "vscode";
+const fs = require("fs");
+
+const CP = ["，", "：", "？", "！", "。", "“", "”", "（", "）"];
+const EP = [",", ":", "?", "!", ".", '"', '"', "(", ")"];
+
 const map = new Map();
 let len = CP.length;
-for(let i =0;i<len;i++){
-	map.set(CP[i],EP[i]);
+for (let i = 0; i < len; i++) {
+  map.set(CP[i], EP[i]);
 }
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(
-	'markdown', {
-		provideDocumentFormattingEdits(document:vscode.TextDocument,
-				options:vscode.FormattingOptions, 
-				token:vscode.CancellationToken){
-			const result: vscode.TextEdit[] = [];
-			const start = new vscode.Position(0, 0);
-			const end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-			const range = new vscode.Range(start, end);
-		    let charArr = document.getText(range).split('');
-			for(let i =0;i<charArr.length;i++){
-				let char = charArr[i];
-				if(map.has(char)){
-					charArr[i] = map.get(char);
-				}
-			}
-			result.push(new vscode.TextEdit(range, charArr.join('')));
-			return result;
-		}
-		}));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vinvincent.replace", () => {
+      let fileName: string | undefined =
+        vscode.window.activeTextEditor?.document.fileName;
+      const text = fs.readFileSync(fileName).toString();
+      let charArr = text.split("");
+      for (let i = 0; i < charArr.length; i++) {
+        let char = charArr[i];
+        if (map.has(char)) {
+          charArr[i] = map.get(char);
+        }
+      }
+      const content = charArr.join("");
+      fs.writeFile(fileName, content, (err: Error) => {
+        if (err) {
+          return;
+        }
+      });
+    })
+  );
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
